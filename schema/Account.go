639c112bf5d4e48fefd0bc84c60cc0823c70b632
Jefry Chiedi <jefrychiedi1999@gmail.com>
2023-06-08 15:46:07 +0700

    Simplification part 2

--- schema/Account.go
+++ schema/Account.go
@@ -10,7 +10,6 @@ type Account struct {
 	Id        string `json:"id" gorm:"primaryKey;size:50;"`
 	UserId    string `json:"user_id"`
 	CreatedAt time.Time
-	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt
 	IsDeleted bool `gorm:"type:bool;default:false" json:"delete"`
 	IsActive  bool `gorm:"type:bool;default:true" json:"status"`
--- schema/User.go
+++ schema/User.go
@@ -1,19 +1,11 @@
 package schema
 
-import (
-	"time"
-
-	"gorm.io/gorm"
-)
-
 type User struct {
-	Id        string `json:"id" gorm:"primaryKey;size:50;"`
-	Email     string `gorm:"size:255;not null;unique" json:"email"`
-	Username  string `gorm:"size:255;not null;unique" json:"username"`
-	Password  string `gorm:"size:255;not null;" json:"-"`
-	CreatedAt time.Time
-	DeletedAt gorm.DeletedAt
-	Accounts  []Account `gorm:"foreignKey:UserId"`
+	Id       string    `json:"id" gorm:"primaryKey;size:50;"`
+	Email    string    `gorm:"size:255;not null;unique" json:"email"`
+	Username string    `gorm:"size:255;not null;unique" json:"username"`
+	Password string    `gorm:"size:255;not null;" json:"-"`
+	Accounts []Account `gorm:"foreignKey:UserId"`
 }
 
 func (User) TableName() string {
