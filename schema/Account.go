1ff41da08aa27525b1e2a70904d50bc4864dc3fc
Jefry Chiedi <jefrychiedi1999@gmail.com>
2023-06-08 16:29:03 +0700

    Simplification part 3

--- schema/Account.go
+++ schema/Account.go
@@ -2,16 +2,12 @@ package schema
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type Account struct {
 	Id        string `json:"id" gorm:"primaryKey;size:50;"`
 	UserId    string `json:"user_id"`
 	CreatedAt time.Time
-	DeletedAt gorm.DeletedAt
-	IsDeleted bool `gorm:"type:bool;default:false" json:"delete"`
 	IsActive  bool `gorm:"type:bool;default:true" json:"status"`
 }
 
