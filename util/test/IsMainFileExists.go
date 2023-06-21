f2d34c2ddac9900410f0edeadfd8061fc6527c6b
Jefry Chiedi <jefrychiedi1999@gmail.com>
2023-06-21 13:44:17 +0700

    CI/CD

---
+++ util/test/IsMainFileExists.go
@@ -0,0 +1,18 @@
+package test
+
+import (
+	"os"
+	"testing"
+)
+
+func IsMainFileExists(t *testing.T) {
+	// Cek apakah file main.go ada di direktori root
+	_, err := os.Stat("../main.go")
+	if err != nil {
+		if os.IsNotExist(err) {
+			t.Fatal("File main.go tidak ditemukan di direktori root")
+		} else {
+			t.Fatalf("Terjadi kesalahan saat memeriksa file main.go: %v", err)
+		}
+	}
+}
